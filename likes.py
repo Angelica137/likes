50727b8fc89872eb24ef12448c87395752e9478a
Angelica <Angelica137@users.noreply.github.com>
2023-12-30 11:06:36 +0000

    one person likes htis, green

--- likes.py
+++ likes.py
@@ -1,3 +1,5 @@
 def likes(names: list) -> str:
     if not names:
         return ('no one likes this')
+    if len(names) == 1:
+        return (f'{names[0]} likes this')
--- test_likes.py
+++ test_likes.py
@@ -7,6 +7,9 @@ class LikesTest(unittest.TestCase):
         names = []
         self.assertEqual(likes(names), 'no one likes this')
 
+    def test_one_person_likes(self):
+        names = ["p"]
+        self.assertEqual(likes(names), "p likes this")
 
 if __name__ == '__main__':
     unittest.main()
